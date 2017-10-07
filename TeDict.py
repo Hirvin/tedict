d079a475cdfe1e8c8858a2bed01a9babd8aa0024
lhvelasc <luishirvin@gmail.com>
2017-10-07 15:57:20 -0500

    Generando structura de archivos para sub y videos

--- TeDict.py
+++ TeDict.py
@@ -11,90 +11,15 @@ from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QMainWindow,
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 
-####### temporal
-_PATH = "sub.srt"
-_STATE_NUM_FRAME = 1
-_STATE_TIME = 2
-_STATE_TXT = 3
+# my imports
+import MyPaquets.subtitles as sub
+
+# My definitions
+MY_PATH = "/home/lhvelasc/Documentos/MisProyectos/Tedict/"
+SUB_PATH = "Subtitles/sub.srt"
+VIDEO_PATH = "Videos/video.mp4"
 
 
-class frame():
-    def __init__(self, num, time, txt):
-        self.num_frame = int(num)
-        self.start = 0
-        self.end = 0
-        self.txt = txt
-        self.get_time(time)
-
-    def cov_milis(self, value):
-        data = value.split(":")
-        h = int(data[0]) * 3600 * 1000
-        m = int(data[1]) * 60 * 1000
-        d2 = data[2].split(',')
-        s = int(d2[0]) * 1000
-        mi = int(d2[1])
-
-        return h + m + s + mi 
-
-    def get_time(self, time):
-        data = time.split(' --> ')
-        self.start = self.cov_milis(data[0])
-        self.end = self.cov_milis(data[1])
-
-    def __str__(self):
-        text = "num: %s \ntiempo: %s - %s\ntext: %s\n" %(str(self.num_frame), self.start, self.end, self.txt)
-        return str(text)
-
-class frames():
-    def __init__(self, path):
-        f = open(path, "r")
-        self.text = f.read()
-        f.close()
-        self.list_frames = []
-        self.get_frame()
-
-    def get_frame(self):
-        self.text = self.text.split("\n")
-
-
-        # eliminando caracte raro del inicio
-        if self.text[0] != '1':
-            self.text[0] = '1'
-
-        state = _STATE_NUM_FRAME
-        for t in self.text:
-            if t != '':
-                if state == _STATE_NUM_FRAME:
-                    num = int(t)
-                    state = _STATE_TIME
-                    continue
-                if state == _STATE_TIME:
-                    time = t
-                    state = _STATE_TXT
-                    continue
-                if state == _STATE_TXT:
-                    txt = t
-                    state = _STATE_NUM_FRAME
-                    self.list_frames.append(frame(num, time, txt))
-                    continue
-
-    def pop(self):
-        return self.list_frames.pop(0)
-
-    def print_frames(self):
-        for f in self.list_frames:
-            print(f)
-
-#####################
-
-
-
-
-
-
-
-# Ruta del archivo.
-VIDEO_PATH = "/home/lhvelasc/Documentos/MisProyectos/Tedict/video.mp4"
 class MainWindow(QMainWindow):
     
     def __init__(self):
@@ -116,14 +41,14 @@ class MainWindow(QMainWindow):
         self.label_sub = QLabel()
 
         # inicializar subtitulos
-        self.list_frames = frames(_PATH)
+        self.list_frames = sub.frames(MY_PATH + SUB_PATH)
         self.frame = self.list_frames.pop()
         
         # Control de reproducción de video de Qt.
         self.video_widget = QVideoWidget(self)
         self.media_player = QMediaPlayer()
         self.media_player.setMedia(
-            QMediaContent(QUrl.fromLocalFile(VIDEO_PATH)))
+            QMediaContent(QUrl.fromLocalFile(MY_PATH + VIDEO_PATH)))
         self.media_player.setVideoOutput(self.video_widget)
         
         # Botones de reproducción y pausa.
